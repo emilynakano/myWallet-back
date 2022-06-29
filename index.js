68663e8ee6476263c4b89730aea259d2207fa8a5
emily <nakanodepaula@gmail.com>
2022-06-29 14:02:47 -0300

    sign-up with verifications and cryptography

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express, {json} from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import bcrypt from 'bcrypt'
+import joi from 'joi'
 
 dotenv.config()
 
@@ -16,8 +18,31 @@ client.connect().then(() => {
 const server = express();
 
 server.use(json())
+
 server.use(cors())
 
-console.log("oi")
+server.post('/sign-up', async(req, res) => {
+    const {name, email, password, chechPassword} = req.body;
+
+    const userSchema = joi.object ({
+        name: joi.string().required(),
+        email: joi.string().email().required(),
+        password: joi.string().required(),
+        chechPassword: joi.any().valid(joi.ref('password')).required()
+    })
+
+    try {
+        const {error} = userSchema.validate(req.body)
+        if(error) {
+            return res.sendStatus(400)
+        }
+        const passwordHash  = bcrypt.hashSync(password, 10);
+        await db.collection('users').insertOne({name, email, password:passwordHash});
+        res.sendStatus(201)
+    } catch {
+        res.sendStatus(500)
+    }
+    
+})
 
 server.listen(5000)
